4f139cc839242fba717cadc2d171ab7c152af94d
marcnavi <marcosreiven@gmail.com>
2022-02-05 20:52:50 -0400

    readme agregado

---
+++ helpers88888/multiplicar.js
@@ -0,0 +1,22 @@
+const fs=require('fs');
+
+const crearArchivo=(base=5)=>{
+
+    console.log('==============');
+    console.log('  Tabla del: ', base);
+    console.log('==============');
+    let salida='';
+    for(let i=1; i<=10;i++)
+    {
+         salida= salida + `${base } x ${ i } =  ${base*i}\n`;
+    }
+    console.log(salida);
+    fs.writeFileSync(`tabla-${base}.txt`, salida);
+    console.log(`tabla-${base}.txt  --  creado`);
+}
+
+module.exports={
+    //funcion: crearArchivo
+    //crearArchivo: crearArchivo o
+    crearArchivo
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Notas
+practica 1
+```
+Opciones:                                                [número] [defecto: 10]PS C:\NODEJS\PRACTI      --help     Muestra ayuda                                        [booleano]
+      --version  Muestra número de versión                            [booleano]
+  -b, --base     Es la base de la tabla                     [número] [requerido]
+  -l, --listar   Muestra la lista en consola              [booleano] [requerido]
+  -h, --hasta    Este es el numero hasta donde quieres la tabla
+  ```
